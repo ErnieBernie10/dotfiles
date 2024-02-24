92839406b7dce13265279ccd13618ee5a41d05a2
ErnieBernie10 <arne.boedt1@gmail.com>
2024-02-24 08:28:35 +0100

    Added zellij

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,9 +5,27 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+config.hide_tab_bar_if_only_one_tab = true
+
+function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Tokyo Night"
+	else
+		return "Tokyo Night Day"
+	end
+end
 
 -- For example, changing the color scheme:
-config.color_scheme = "Tokyo Night"
+config.color_scheme = scheme_for_appearance(get_appearance())
 
 config.window_padding = {
 	left = 0,
