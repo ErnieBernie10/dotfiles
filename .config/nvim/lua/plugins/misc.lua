b1005bcaf5f423d6e6ca0ebf852852ba848b02f3
ErnieBernie10 <arne.boedt1@gmail.com>
2024-03-28 21:33:57 +0100

    Updated lualine and lazygit

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -2,6 +2,10 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     opts = {
+      options = {
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "|", right = "|" },
+      },
       sections = {
         lualine_b = { "branch", "harpoon2" },
       },
