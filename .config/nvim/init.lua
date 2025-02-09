e64cb6fc030fc6ea44411fcd1f8aba4790fc698a
ErnieBernie10 <arne.boedt1@gmail.com>
2025-02-09 16:03:32 +0100

    feat: migrate nvim to vscode configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,90 @@
--- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-require('keymap')
-require('autocmd')
-require('options')
+local vscode = require('vscode')
 
-local path_package = vim.fn.stdpath('data') .. '/site/'
-local mini_path = path_package .. 'pack/deps/start/mini.nvim'
-if not vim.loop.fs_stat(mini_path) then
-    vim.cmd('echo "Installing `mini.nvim`" | redraw')
-    local clone_cmd = { 'git', 'clone', '--filter=blob:none', 'https://github.com/echasnovski/mini.nvim', mini_path }
-    vim.fn.system(clone_cmd)
-    vim.cmd('packadd mini.nvim | helptags ALL')
-    vim.cmd('echo "Installed `mini.nvim`" | redraw')
+vim.g.mapleader = " "
+-- highlight on yank
+vim.cmd("au TextYankPost * lua vim.highlight.on_yank {higroup='IncSearch', timeout=200}")
+
+vim.o.tabstop = 4 -- Number of spaces that a <Tab> in the file counts for
+vim.o.shiftwidth = 4 -- Number of spaces to use for each step of (auto)indent
+vim.o.expandtab = true -- Use spaces instead of tabs
+vim.o.relativenumber = true
+vim.wo.cursorline = true
+vim.wo.scrolloff = 10
+
+-- lsp
+vim.keymap.set("n", "<leader>cl", "<cmd>LspInfo<cr>", {
+    desc = "Lsp Info"
+})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
+    desc = "Goto Definition"
+})
+vim.keymap.set("n", "gr", vim.lsp.buf.references, {
+    desc = "References",
+    nowait = true
+})
+vim.keymap.set("n", "gI", vim.lsp.buf.implementation, {
+    desc = "Goto Implementation"
+})
+vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, {
+    desc = "Goto T[y]pe Definition"
+})
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {
+    desc = "Goto Declaration"
+})
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {
+    desc = "Hover"
+})
+vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, {
+    desc = "Signature Help"
+})
+vim.keymap.set("i", "<c-k>", vim.lsp.buf.signature_help, {
+    desc = "Signature Help"
+})
+vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {
+    desc = "Code Action"
+})
+vim.keymap.set({"n", "v"}, "<leader>cc", vim.lsp.codelens.run, {
+    desc = "Run Codelens"
+})
+vim.keymap.set("n", "<leader>cC", vim.lsp.codelens.refresh, {
+    desc = "Refresh & Display Codelens"
+})
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {
+    desc = "Rename"
+})
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, {
+    desc = "Show Line Diagnostics"
+})
+-- vscode
+-- Search files in the project
+function quickOpen()
+    vscode.action("workbench.action.quickOpen")
 end
 
--- Set up 'mini.deps' (customize to your liking)
-require('mini.deps').setup({ path = { package = path_package } })
+vim.keymap.set("n", "<leader>ff", quickOpen, {
+    desc = "Quick open"
+})
+vim.keymap.set("n", "<leader> ", quickOpen, {
+    desc = "Quick open"
+})
 
-require('plugins')
+-- open explorer
+function openExplorer()
+    vscode.action("workbench.view.explorer")
+end
+vim.keymap.set("n", "<leader>e", openExplorer, {
+    desc = "Explorer"
+})
+
+function gotoLeftTab()
+    vscode.action("workbench.action.previousEditor")
+end
+function gotoRightTab()
+    vscode.action("workbench.action.nextEditor")
+end
+vim.keymap.set("n", "<C-h>", gotoLeftTab, {
+    desc = "Previous Tab"
+})
+vim.keymap.set("n", "<C-l>", gotoRightTab, {
+    desc = "Next Tab"
+})
